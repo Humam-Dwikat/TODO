bfbaa37d79bccb7380cb6ce537e9601ae14ee24e
omarhal <96300201+omarhal@users.noreply.github.com>
2021-12-17 22:36:41 +0100

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -15,3 +15,21 @@ addTextFromInput.addEventListener("keypress", (e) => { //when users press on th 
 	    addTextFromInput.value = "";//when the user press Enter the input box = NULL
 	}
 });
+const shapeForChangeMode =document.getElementById("change_mode"); 
+
+shapeForChangeMode.addEventListener("click",(e)=>{
+	let shape =document.body;
+    shape.classList.toggle("light_mode");
+    console.log("holle");
+    var image = document.getElementById("myImage");
+    if (image.src.match("266998931_1061886381053204_303416707441577145_n.jpg")) {
+        image.src = "WhatsApp Image 2021-12-17 at 7.55.43 AM.jpeg";
+    }
+    else {
+        image.src = "266998931_1061886381053204_303416707441577145_n.jpg";
+    }
+
+
+});
+
+
