478d4d2fd19a8d507abb7e8c05d566cc95dbdcd9
omarhal <96300201+omarhal@users.noreply.github.com>
2021-12-17 22:01:48 +0100

    Create css.h

---
+++ css.h
@@ -0,0 +1,73 @@
+.container{
+    position: relative;
+   
+  
+}
+.container >img {
+    width: 100%;
+    height: 300px;
+    
+   
+  
+}
+body {
+    background-color: #181824;
+    color: white;
+    margin: 0;
+    padding: 0;
+    text-align: center;
+    
+  
+
+   
+}
+.image{
+    margin-top: 100px;
+    position:absolute;
+    top: 50%;
+    right: 50%;
+    display: flex;
+    justify-content: space-between;
+    margin: -200px;
+    padding: 20px;
+    margin-left: 200px;
+    color: white;
+}
+.sun{
+    margin: -1px;
+    padding: 20px;
+    margin-left: 219px;
+    color: white;
+
+}
+
+.functions{/*size the font in the div founctions*/
+    font-size: 80%;
+    border:2px solid #25273c;
+    background-color: #25273c;
+    border-radius: 8px;
+    margin: 10% 25% 10% 20%;
+
+
+}
+.filter{/* put border and background color to box founction*/
+    display:inline-flex;
+
+    border:2px solid #25273c;
+    background-color: #25273c;
+    border-radius: 8px;
+}
+
+.three_function{/* put spaces between the founctions name
+    */
+    margin-left: 30px;
+    margin-right: 20px;
+    
+}
+ 
+.light_mode{
+    background-color:white;
+    color:black;
+
+
+}
