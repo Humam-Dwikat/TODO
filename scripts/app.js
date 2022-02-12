b1fb8a23b532a115cea4c6858dcdda2b8e7b74d4
Humam-Dwikat <humamdwik@gmail.com>
2022-02-12 13:09:41 +0200

    add eventlistner on tasks

--- scripts/app.js
+++ scripts/app.js
@@ -34,6 +34,7 @@ addTextFromInput.addEventListener("keypress", (e) => {
             //creat a new div 
             check.classList.toggle("checkBox");
             //div has a class
+            let num =check.id=tasks.length;
             task.appendChild(check);
             //put the div(check) in the ((task)= task div)
             task.appendChild(text);
@@ -41,6 +42,15 @@ addTextFromInput.addEventListener("keypress", (e) => {
              //task div end
             tasks_list.appendChild(task);
             //put the task(check,text) in the tasks_list
+            if (tasks.length>0) {
+                const w=document.getElementById(tasks.length);
+                   w.addEventListener("click",function () {
+                        tasks[(num-1)].iscompleted=0;
+                            countActiveTask();
+                      }
+                      )
+          }
+                
 	  }
 	    addTextFromInput.value = "";
         //when the user press Enter the input box = NULL
@@ -68,6 +78,8 @@ button.addEventListener("click", function(){
             //creat a new div 
             check.classList.toggle("checkBox");
             //div has a class
+             check.id=tasks.length;
+             let num =check.id=tasks.length;
             task.appendChild(check);
             //put the div(check) in the ((task)= task div)
             task.appendChild(text);
@@ -75,11 +87,25 @@ button.addEventListener("click", function(){
              //task div end
             tasks_list.appendChild(task);
             //put the task(check,text) in the tasks_list
+            if (tasks.length>0) {
+                const a=document.getElementById(tasks.length);
+                   a.addEventListener("click",function() {    
+                    tasks[(num-1)].iscompleted=0;
+                         countActiveTask();
+                   }
+                    
+                      )
+          }
+                
+          console.log( tasks);
+            
 	  }
 	    addTextFromInput.value = "";
         //when the user press Enter the input box = NULL
         countActiveTask();
 });
+
+
 const shapeForChangeMode =document.getElementById("change_mode"); 
 shapeForChangeMode.addEventListener("click",()=>{
 	let shape =document.body;
@@ -98,7 +124,7 @@ function countActiveTask() {
     let counter = document.querySelectorAll(".counter");
     let CounterTasks = 0;
     for (let i = 0; i < tasks.length; i++) {
-        if (tasks[i].iscompleted) {
+        if (tasks[i].iscompleted==1) {
             //if status of tasks[i] =1= active 
             CounterTasks += 1;
         }
@@ -108,13 +134,23 @@ function countActiveTask() {
     }
 }
 //Humam
+
 function clean(){
 //this function  to cleaner for tasks completed
 for(let i=0;i<tasks.length;i++){
      if(!tasks[i].iscompleted){
-        delete tasks[i];
+         tasks[i].iscompleted=-1;
         }
     }   
 }
 const taskCompleted=document.getElementById("clear");
 taskCompleted.addEventListener("click",clean);
+
+const activetaske=document.getElementById("All");
+activetaske.addEventListener("click",function() {
+    for(let i=0;i<tasks.length;i++){
+        if(tasks[i].iscompleted==1||tasks[i].iscompleted==0){
+            document.getElementById((tasks.length)-1).innerHTM=tasks[i];
+        }
+    }
+})
